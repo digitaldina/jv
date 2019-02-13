1f16b4afefb5bc28b5eed929c1c2e1a09f3c7b17
dina <dinaelhanan@gmail.com>
2019-02-13 15:30:43 -0500

    added hackclub banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,13 @@ import React from "react"
 import logo from "../../static/logo.png"
 
 function Image(){
-    return <div ><a href="https://docs.google.com/forms/d/13u5CvxGbdoKRnrjp16H5MDlfXjnRJm6hy81H6VX-9x0">
+    return <div >
+      <a href="https://hackclub.com/"><img style={{position: `absolute`, top: `10px`, left: 0, border: 0, width: `230px`, zIndex: 999}} src="https://cdn.jsdelivr.net/gh/hackclub/meta@fc4c0d220426eb53a176e656ae7700d9dcd0b2c5/logos/banner_orpheus_hand.svg" alt="Hack Club"/></a>
+      <a href="https://docs.google.com/forms/d/13u5CvxGbdoKRnrjp16H5MDlfXjnRJm6hy81H6VX-9x0">
     <img src={logo} alt="Kid typing behind laptop,
      with Hack Club, JV and a get a reminder sticker.
-     click for link that sends you to set a reminder for 
-      next meeting"/></a>
-     
-        
+     click for link that sends you to join"/></a>
+
         <div>
           {/* © {new Date().getFullYear()}, */}
            Built with
