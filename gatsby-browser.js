50a61044394373c1090a90e10b2a1de7e571d5aa
dina <dinaelhanan@gmail.com>
2019-02-13 13:54:24 -0500

    created the site all in one go~!

---
+++ gatsby-browser.js
@@ -0,0 +1,5 @@
+import "./src/styles/global.css"
+
+// or:
+// require('./src/styles/global.css')
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import logo from "../../static/logo.png"
 
-export default () => <div>Hello world!</div>
+function Image(){
+    return <div ><a href="https://docs.google.com/forms/d/13u5CvxGbdoKRnrjp16H5MDlfXjnRJm6hy81H6VX-9x0">
+    <img src={logo} alt="Kid typing behind laptop,
+     with Hack Club, JV and a get a reminder sticker.
+     click for link that sends you to set a reminder for 
+      next meeting"/></a></div>
+}
+
+export default Image
