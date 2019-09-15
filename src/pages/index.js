ebc834aec995274c7aa2bd266f7a6c303cc7bead
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-09-15 19:58:50 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ function Image(){
      with Hack Club, JV and a get a reminder sticker.
      click for link that sends you to join"/></a>
     </div>
+    <p style={{fontFamily: 'Segoe UI', color: "white"}}>Got a suggestion for a workshop you want to see at JV's Hack Club? <a href="https://docs.google.com/forms/d/18KIrhHSD9QemtvMccrNzO16kk3DvdRLOVQQn53-6Vy4" style={{color: "#9586EA"}}>Let us know here!</a></p>
 }
 
 export default Image
