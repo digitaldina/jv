68f36c8b112291c07ca9581e2b067e38ff2c4c93
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-09-15 20:30:42 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ function Image(){
         <img src={logo}  alt="Kid typing behind laptop,
          with Hack Club, JV and a get a reminder sticker.
          click for link that sends you to join"/></a>
-        <p style={{fontFamily: 'Segoe UI', color: "white"}}>Got a suggestion for a workshop you want to see at JV's Hack Club? <a href="https://docs.google.com/forms/d/18KIrhHSD9QemtvMccrNzO16kk3DvdRLOVQQn53-6Vy4" style={{color: "#9586EA"}}>Let us know here!</a></p>
+        <p style={{fontFamily: 'Segoe UI', color: "#18303B"}}>Got a suggestion for a workshop you want to see at JV's Hack Club? <a href="https://docs.google.com/forms/d/18KIrhHSD9QemtvMccrNzO16kk3DvdRLOVQQn53-6Vy4" style={{color: "#9586EA"}}>Let us know here!</a></p>
     </div>
 }
 
