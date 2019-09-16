05e8ae57170cbf6a0c7100c2cacecb0c253cbd61
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-09-15 20:01:02 -0400

    Add suggestion link for workshops

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import logo from "../../static/logo.png"
 import Banner from '@hackclub/banner'
 function Image(){
     return <div >
-      <Banner />
-      <a href="https://docs.google.com/forms/d/13u5CvxGbdoKRnrjp16H5MDlfXjnRJm6hy81H6VX-9x0">
-    <img src={logo}  alt="Kid typing behind laptop,
-     with Hack Club, JV and a get a reminder sticker.
-     click for link that sends you to join"/></a>
+       
+        <Banner />
+        <a href="https://docs.google.com/forms/d/13u5CvxGbdoKRnrjp16H5MDlfXjnRJm6hy81H6VX-9x0">
+        <img src={logo}  alt="Kid typing behind laptop,
+         with Hack Club, JV and a get a reminder sticker.
+         click for link that sends you to join"/></a>
+        <p style={{fontFamily: 'Segoe UI', color: "white"}}>Got a suggestion for a workshop you want to see at JV's Hack Club? <a href="https://docs.google.com/forms/d/18KIrhHSD9QemtvMccrNzO16kk3DvdRLOVQQn53-6Vy4" style={{color: "#9586EA"}}>Let us know here!</a></p>
     </div>
-    <p style={{fontFamily: 'Segoe UI', color: "white"}}>Got a suggestion for a workshop you want to see at JV's Hack Club? <a href="https://docs.google.com/forms/d/18KIrhHSD9QemtvMccrNzO16kk3DvdRLOVQQn53-6Vy4" style={{color: "#9586EA"}}>Let us know here!</a></p>
 }
 
 export default Image
