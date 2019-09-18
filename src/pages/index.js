c1fc056b18699f762a71c7f4cb37696179cc13fe
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-09-18 07:06:50 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import logo from "../../static/logo.png"
 import Banner from '@hackclub/banner'
 function Image(){
     return <div >
-        <p style={{fontFamily: 'Segoe UI', color: "#18303B",fontSize: 16px}}>Interested in attending the Hack Club Trip to Ryerson in Downtown Toronto for a cybersecurity event? <a href="https://drive.google.com/file/d/0B1AzkdN64xEDOFdPekJJX2lkWW9ET2tISUpobnlfSTBqazE0/view?usp=sharing" style={{color: "#9586EA"}}>Click here for forms</a> (please hand in your forms to Mr. Peters by Friday  Sept. 20th)<a href="https://dmz.ryerson.ca/canhack" style={{color: "#9586EA"}}>Click here for details about the event</a></p>
+        <p style={{fontFamily: 'Segoe UI', color: "#18303B",fontSize: '16px'}}>Interested in attending the Hack Club Trip to Ryerson in Downtown Toronto for a cybersecurity event? <a href="https://drive.google.com/file/d/0B1AzkdN64xEDOFdPekJJX2lkWW9ET2tISUpobnlfSTBqazE0/view?usp=sharing" style={{color: "#9586EA"}}>Click here for forms</a> (please hand in your forms to Mr. Peters by Friday  Sept. 20th)<a href="https://dmz.ryerson.ca/canhack" style={{color: "#9586EA"}}>Click here for details about the event</a></p>
         <a href="https://docs.google.com/forms/d/13u5CvxGbdoKRnrjp16H5MDlfXjnRJm6hy81H6VX-9x0">
         <img src={logo}  alt="Kid typing behind laptop,
          with Hack Club, JV and a get a reminder sticker.
