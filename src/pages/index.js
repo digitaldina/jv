d9c9aeaa13d436129685af64b149b665a4771fab
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-09-18 19:17:52 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import logo from "../../static/logo.png"
+import logo from "../../static/logo.svg"
 import Banner from '@hackclub/banner'
 function Image(){
     return <div >
