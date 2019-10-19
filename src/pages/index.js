a9050334a28fa1bf62ab53c4729c5ea9a847beed
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-10-19 11:41:58 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import logo from "../../static/logo.svg"
 import Banner from '@hackclub/banner'
 function Image(){
     return <div >
+        <p style={{fontFamily: 'Segoe UI', color: "#18303B"}}>Join our<a href="https://discord.gg/XjebnbC" style={{color: "#9586EA"}}> Discord!</a></p>
         <a href="https://docs.google.com/forms/d/13u5CvxGbdoKRnrjp16H5MDlfXjnRJm6hy81H6VX-9x0">
         <img src={logo} style={{padding: '1', margin: '0'}}  alt="Kid typing behind laptop,
          with Hack Club, JV and a get a reminder sticker.
